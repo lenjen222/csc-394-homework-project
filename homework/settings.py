9b3603be3d6857c14198529aea66c13874e84c01
lenjen222 <lenjensen222@gmail.com>
2023-01-18 22:13:11 -0600

    Django now works

--- homework/settings.py
+++ homework/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'J@demaster56',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
