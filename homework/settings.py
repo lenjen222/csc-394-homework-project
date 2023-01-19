db8826d1f6c358b6b455a28b509d9e6d59b7b8d5
lenjen222 <lenjensen222@gmail.com>
2023-01-18 21:12:21 -0600

    Django now works

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^)n3dafo033%^^b)#nz^1!ywm)m(u_wtzu89di5$sp4+z*j_b3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
